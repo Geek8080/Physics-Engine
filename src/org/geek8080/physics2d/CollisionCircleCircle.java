07ad5af3b6f33df76a9f72bdb12af116a8de05d9
Geek8080 <prashantprakash97@#gmail.com>
2020-07-10 15:38:32 +0530

    Added Manifold generation for Circle vs. Circle

--- src/org/geek8080/physics2d/CollisionCircleCircle.java
+++ src/org/geek8080/physics2d/CollisionCircleCircle.java
@@ -1,13 +1,39 @@
 package org.geek8080.physics2d;
 
-public class CollisionCircleCircle implements CollisionCallback{
+public class CollisionCircleCircle implements CollisionCallback {
 
-    public static final CollisionCircleCircle instance = new CollisionCircleCircle();
+	public static final CollisionCircleCircle instance = new CollisionCircleCircle();
 
-    @Override
-    public void handleCollision(Manifold m, Body body1, Body body2) {
-        // TODO Auto-generated method stub
+	@Override
+	public void handleCollision(Manifold m, Body body1, Body body2) {
+		Circle a = (Circle) body1.shape;
+		Circle b = (Circle) body2.shape;
 
-    }
-    
+		Vector2D normal = Vector2D.differenceV(body2.position, body1.position);
+
+		float dist_sqr = normal.lengthSq();
+		float radius = a.radius + b.radius;
+
+		if (dist_sqr >= radius * radius) {
+			m.contactCount = 0;
+			return;
+		}
+
+		float distance = (float) StrictMath.sqrt(dist_sqr);
+
+		m.contactCount = 1;
+
+		if (distance == 0.0f) {
+			m.penetration = a.radius;
+			m.normal.set(1.0f, 1.0f);
+			m.contacts[0].set(body1.position);
+		} else {
+			m.penetration = radius - distance;
+			// Faster than using Normalized since we already performed sqrt
+			m.normal.set(normal);
+			m.normal.scaledDivision(distance);
+			m.contacts[0].set(Vector2D.sumV(Vector2D.scaledMultiplicationN(m.normal, a.radius), body1.position));
+		}
+	}
+
 }
--- src/org/geek8080/physics2d/Manifold.java
+++ src/org/geek8080/physics2d/Manifold.java
@@ -2,4 +2,39 @@ package org.geek8080.physics2d;
 
 public class Manifold {
 
+    public Body a;
+    public Body b;
+    public float penetration;
+    public final Vector2D normal = new Vector2D();
+    public Vector2D[] contacts = { new Vector2D(), new Vector2D() };
+    public float contactCount;
+    public float e;
+    public float sf;
+    public float df;
+
+    public Manifold(Body a, Body b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public void solve() {
+
+    }
+
+    public void initialize() {
+
+    }
+
+    public void applyImpulse() {
+
+    }
+
+    public void positionalCorrection() {
+
+    }
+
+    public void infinitemassCorrection() {
+
+    }
+
 }
