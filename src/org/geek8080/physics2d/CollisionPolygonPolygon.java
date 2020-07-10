60cae1bcf01bd40c5a7f5d11dcf28388be65c7c3
Geek8080 <prashantprakash97@#gmail.com>
2020-07-10 22:18:37 +0530

    Added manifold generation for Polygon vs. Polygon

--- src/org/geek8080/physics2d/CollisionPolygonPolygon.java
+++ src/org/geek8080/physics2d/CollisionPolygonPolygon.java
@@ -1,12 +1,183 @@
 package org.geek8080.physics2d;
 
-public class CollisionPolygonPolygon implements CollisionCallback{
-    
-    public static final CollisionPolygonPolygon instance = new CollisionPolygonPolygon();
+public class CollisionPolygonPolygon implements CollisionCallback {
 
-    @Override
-    public void handleCollision(Manifold m, Body body1, Body body2) {
-        // TODO Auto-generated method stub
+	public static final CollisionPolygonPolygon instance = new CollisionPolygonPolygon();
 
-    }
+	@Override
+	public void handleCollision(Manifold m, Body body1, Body body2) {
+		Polygon a = (Polygon) body1.shape;
+		Polygon b = (Polygon) body2.shape;
+		m.contactCount = 0;
+
+		int[] faceA = { 0 };
+		float penetrationA = findLeastPenetrationAxis(faceA, a, b);
+		if (penetrationA >= 0.0f) {
+			return;
+		}
+
+		int[] faceB = { 0 };
+		float penetrationB = findLeastPenetrationAxis(faceB, b, a);
+		if (penetrationB >= 0.0f) {
+			return;
+		}
+
+		int referenceIndex;
+		boolean flip;
+
+		Polygon refPoly;
+		Polygon incPoly;
+
+		if (ImpulseMath.gt(penetrationA, penetrationB)) {
+			refPoly = a;
+			incPoly = b;
+			referenceIndex = faceA[0];
+			flip = false;
+		} else {
+			refPoly = b;
+			incPoly = a;
+			referenceIndex = faceB[0];
+			flip = true;
+		}
+
+		Vector2D[] incidentFace = Vector2D.arrayOf(2);
+
+		findIncidentFace(incidentFace, refPoly, incPoly, referenceIndex);
+
+		Vector2D v1 = refPoly.vertices[referenceIndex];
+		referenceIndex = referenceIndex + 1 == refPoly.vertexCount ? 0 : referenceIndex + 1;
+		Vector2D v2 = refPoly.vertices[referenceIndex];
+
+		v1 = Vector2D.sumV(refPoly.u.mul(v1), refPoly.body.position);
+		v2 = Vector2D.sumV(refPoly.u.mul(v2), refPoly.body.position);
+
+		Vector2D sidePlaneNormal = Vector2D.differenceV(v2, v1);
+		sidePlaneNormal.normalize();
+
+		// Orthogonalize
+		Vector2D refFaceNormal = new Vector2D(sidePlaneNormal.y, -sidePlaneNormal.x);
+
+		float refC = Vector2D.dotProduct(refFaceNormal, v1);
+		float negSide = -Vector2D.dotProduct(sidePlaneNormal, v1);
+		float posSide = Vector2D.dotProduct(sidePlaneNormal, v2);
+
+		if (clip(Vector2D.scaledMultiplicationN(sidePlaneNormal, -1), negSide, incidentFace) < 2) {
+			return;
+		}
+
+		if (clip(sidePlaneNormal, posSide, incidentFace) < 2) {
+			return;
+		}
+
+		m.normal.set(refFaceNormal);
+		if (flip) {
+			m.normal.scaledMultiplication(-1);
+		}
+
+		int cp = 0;
+		float seperation = Vector2D.dotProduct(refFaceNormal, incidentFace[0]) - refC;
+		if (seperation <= 0.0f) {
+			m.contacts[cp].set(incidentFace[0]);
+			m.penetration = -seperation;
+			++cp;
+		} else {
+			m.penetration = 0;
+		}
+
+		seperation = Vector2D.dotProduct(refFaceNormal, incidentFace[1]) - refC;
+		if (seperation <= 0.0f) {
+			m.contacts[cp].set(incidentFace[1]);
+
+			m.penetration += -seperation;
+			++cp;
+
+			m.penetration /= cp;
+		}
+
+		m.contactCount = cp;
+
+	}
+
+	private int clip(Vector2D n, float c, Vector2D[] face) {
+
+		int sp = 0;
+		Vector2D[] out = { new Vector2D(face[0]), new Vector2D(face[1]) };
+
+		float d1 = Vector2D.dotProduct(n, face[0]) - c;
+		float d2 = Vector2D.dotProduct(n, face[1]) - c;
+
+		if (d1 <= 0.0f) {
+			out[sp++].set(face[0]);
+		}
+
+		if (d2 <= 0.0f) {
+			out[sp++].set(face[1]);
+		}
+
+		if (d1 * d2 < 0.0f) {
+			float alpha = d1 / (d1 - d2);
+			out[sp] = Vector2D.sumV(face[0],
+					Vector2D.scaledMultiplicationN(Vector2D.differenceV(face[1], face[0]), alpha));
+			++sp;
+		}
+
+		face[0] = out[0];
+		face[1] = out[1];
+
+		return sp;
+	}
+
+	private void findIncidentFace(Vector2D[] v, Polygon refPoly, Polygon incPoly, int referenceIndex) {
+
+		Vector2D referenceNormal = refPoly.normals[referenceIndex];
+
+		referenceNormal = refPoly.u.mul(referenceNormal);
+		referenceNormal = incPoly.u.transpose().mul(referenceNormal);
+
+		int incidentFace = 0;
+		float minDot = Float.MAX_VALUE;
+
+		for (int i = 0; i < incPoly.vertexCount; i++) {
+			float dot = Vector2D.dotProduct(referenceNormal, incPoly.normals[i]);
+
+			if (dot < minDot) {
+				minDot = dot;
+				incidentFace = i;
+			}
+		}
+
+		v[0] = Vector2D.sumV(incPoly.u.mul(incPoly.vertices[incidentFace]), incPoly.body.position);
+		incidentFace = incidentFace + 1 >= (int) incPoly.vertexCount ? 0 : incidentFace + 1;
+		v[1] = Vector2D.sumV(incPoly.u.mul(incPoly.vertices[incidentFace]), incPoly.body.position);
+
+	}
+
+	private float findLeastPenetrationAxis(int[] faceIndex, Polygon A, Polygon B) {
+		float bestDistance = -Float.MAX_VALUE;
+		int bestIndex = 0;
+
+		for (int i = 0; i < A.vertexCount; i++) {
+			Vector2D nw = A.u.mul(A.normals[i]);
+
+			Mat22 buT = B.u.transpose();
+			Vector2D n = buT.mul(nw);
+
+			Vector2D s = B.getSupport(Vector2D.scaledMultiplicationN(n, -1));
+
+			Vector2D v = A.vertices[i];
+			v = Vector2D.sumV(A.u.mul(v), A.body.position);
+			v.difference(B.body.position);
+			v = buT.mul(v);
+
+			float d = Vector2D.dotProduct(n, Vector2D.differenceV(s, v));
+
+			if (d > bestDistance) {
+				bestDistance = d;
+				bestIndex = i;
+			}
+		}
+
+		faceIndex[0] = bestIndex;
+		return bestDistance;
+	}
 }
--- src/org/geek8080/physics2d/Mat22.java
+++ src/org/geek8080/physics2d/Mat22.java
@@ -1,6 +1,5 @@
 package org.geek8080.physics2d;
 
-
 /**
  * The class handles Vector rotation
  */
@@ -61,6 +60,74 @@ public class Mat22 {
         this.m01 = m.m01;
         this.m10 = m.m10;
         this.m11 = m.m11;
+    }
+
+    /**
+     * Returns a new vector that is the transformation of v by this matrix.
+     */
+    public Vector2D mul(Vector2D v) {
+        return mul(v.x, v.y, new Vector2D());
+    }
+
+    /**
+     * Sets out the to transformation of {x,y} by this matrix.
+     */
+    public Vector2D mul(float x, float y, Vector2D out) {
+        out.x = m00 * x + m01 * y;
+        out.y = m10 * x + m11 * y;
+        return out;
+    }
+
+    /**
+     * Multiplies this matrix by x.
+     */
+    public void muli(Mat22 x) {
+        set(m00 * x.m00 + m01 * x.m10, m00 * x.m01 + m01 * x.m11, m10 * x.m00 + m11 * x.m10, m10 * x.m01 + m11 * x.m11);
+    }
+
+    /**
+     * Sets out to the multiplication of this matrix and x.
+     */
+    public Mat22 mul(Mat22 x, Mat22 out) {
+        out.m00 = m00 * x.m00 + m01 * x.m10;
+        out.m01 = m00 * x.m01 + m01 * x.m11;
+        out.m10 = m10 * x.m00 + m11 * x.m10;
+        out.m11 = m10 * x.m01 + m11 * x.m11;
+        return out;
+    }
+
+    /**
+     * Returns a new matrix that is the multiplication of this and x.
+     */
+    public Mat22 mul(Mat22 x) {
+        return mul(x, new Mat22());
+    }
+
+    /**
+     * Sets the matrix to it's transpose.
+     */
+    public void transposei() {
+        float t = m01;
+        m01 = m10;
+        m10 = t;
+    }
+
+    /**
+     * Sets out to the transpose of this matrix.
+     */
+    public Mat22 transpose(Mat22 out) {
+        out.m00 = m00;
+        out.m01 = m10;
+        out.m10 = m01;
+        out.m11 = m11;
+        return out;
+    }
+
+    /**
+     * Returns a new matrix that is the transpose of this matrix.
+     */
+    public Mat22 transpose() {
+        return transpose(new Mat22());
     }
 
 }
