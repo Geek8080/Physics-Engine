6b6e094c54ed4928c5857ec2fe47a9f06cc006e4
Geek8080 <prashantprakash97@#gmail.com>
2020-07-11 12:35:19 +0530

    Added manifold generation for Polygon vs. Circle

--- src/org/geek8080/physics2d/CollisionPolygonCircle.java
+++ src/org/geek8080/physics2d/CollisionPolygonCircle.java
@@ -1,12 +1,15 @@
 package org.geek8080.physics2d;
 
-public class CollisionPolygonCircle implements CollisionCallback{
-    
+public class CollisionPolygonCircle implements CollisionCallback {
+
     public static final CollisionPolygonCircle instance = new CollisionPolygonCircle();
 
     @Override
     public void handleCollision(Manifold m, Body body1, Body body2) {
-        // TODO Auto-generated method stub
+        CollisionCirclePolygon.instance.handleCollision(m, body2, body1);
 
+        if (m.contactCount > 0) {
+            m.normal.set(Vector2D.scaledMultiplicationN(m.normal, -1));
+        }
     }
 }
